d06d9ef7bb2a61ce6e95a75c6a0244aa3359b39d
ElodieDulis <elodie.dulis@gmail.com>
2020-12-23 22:43:07 +0100

    feat: add guessing mechanism

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-guessedNumber = input("Try to enter a number between 1 and 100 \n5")
+import numpy as np
+guessedNumber = input("Try to enter a number between 1 and 100 \n")
 print("Your first guess is : " + guessedNumber)
+magicNumber = np.random.randint(100)
+print("The magic number is : " + str(magicNumber))
+
