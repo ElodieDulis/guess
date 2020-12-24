f73a4327d857173d0bef55189d8194a7230c6331
ElodieDulis <elodie.dulis@gmail.com>
2020-12-24 17:44:32 +0100

    feat(game): complete game

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np
-guessedNumber = input("Try to enter a number between 1 and 100 \n")
-print("Your first guess is : " + guessedNumber)
+# Game start 
 magicNumber = np.random.randint(100)
-print("The magic number is : " + str(magicNumber))
+guessedNumber = int(input("Try to enter a number between 1 and 100 \n"))
+# Guessing process
+while (magicNumber != guessedNumber):
+    if magicNumber > guessedNumber : 
+        guessedNumber = int(input("Your number is too low ! try again \n"))
+    else:
+        guessedNumber = int(input("Your number is too high ! try again \n"))
+# Game end 
+print("Good job ! The magic number is : " + str(magicNumber))
 
